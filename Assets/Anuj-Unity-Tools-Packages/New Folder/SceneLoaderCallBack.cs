16dcbeb66552183fdb86e009aec16369026d197a
Anuj Chouhan <anujchouhan112@gmail.com>
2025-07-06 00:00:31 +0530

    More Experiments

---
+++ Assets/Anuj-Unity-Tools-Packages/New Folder/SceneLoaderCallBack.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoaderCallBack : MonoBehaviour
+{
+    private bool isFirstUpdate = true;
+
+    private void Update()
+    {
+        if (isFirstUpdate)
+        {
+            isFirstUpdate = false;
+
+            // Check if there is a pending scene load request
+            if (SceneLoader.HasPendingLoad())
+            {
+                SceneLoader.LoaderCallBack();
+            }
+            else
+            {
+                // If no scene was specified, assume this is the first startup and load the main menu or default gameplay scene
+                SceneLoader.LoadScene(SceneLoader.Scenes.Gameplay);
+            }
+        }
+    }
+}
