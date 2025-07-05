ea75f0589832e99bc29d33b65dabad3a71a8754c
Anuj Chouhan <anujchouhan112@gmail.com>
2025-07-05 19:22:06 +0530

    Added interface

--- Assets/My-Unity-Tools/Save-Load-Manager/Runtime/Parent.cs
+++ Assets/My-Unity-Tools/Save-Load-Manager/Runtime/Parent.cs
@@ -3,9 +3,18 @@ using UnityEngine;
 namespace SaveLoad
 {
 
-    public abstract class Parent : MonoBehaviour
+    public abstract class Parent : MonoBehaviour, MyInterface
     {
 
     }
 
+}
+
+public interface MyInterface
+{
+
+    public void Method()
+    {
+
+    }
 }
